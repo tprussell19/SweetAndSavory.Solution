d7eca65fc2629eb5ae67db66824ced0550d6d94d
Thomas Russell <t.p.russell19@gmail.com>
2021-06-04 12:00:22 -0700

    :hammer: add authorization requirements and add and delete flavor routes

--- SweetAndSavory/Controllers/TreatsController.cs
+++ SweetAndSavory/Controllers/TreatsController.cs
@@ -21,12 +21,13 @@ namespace SweetAndSavory.Controllers
       return View(model);
     }
 
+    [Authorize]
     public ActionResult Create()
     {
-      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name", new { @class = "custom-select" });
       return View();
     }
 
+    [Authorize]
     [HttpPost]
     public ActionResult Create(Treat treat)
     {
@@ -37,10 +38,14 @@ namespace SweetAndSavory.Controllers
 
     public ActionResult Details(int id)
     {
-      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      var thisTreat = _db.Treats
+          .Include(treat => treat.JoinEntities)
+          .ThenInclude(join => join.Machine)
+          .FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
 
+    [Authorize]
     public ActionResult Edit(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
@@ -48,6 +53,7 @@ namespace SweetAndSavory.Controllers
       return View(thisTreat);
     }
 
+    [Authorize]
     [HttpPost]
     public ActionResult Edit(Treat treat)
     {
@@ -56,12 +62,14 @@ namespace SweetAndSavory.Controllers
       return RedirectToAction("Index");
     }
 
+    [Authorize]
     public ActionResult Delete(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
 
+    [Authorize]
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
@@ -69,6 +77,44 @@ namespace SweetAndSavory.Controllers
       _db.Treats.Remove(thisTreat);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    [Authorize]
+    public ActionResult AddFlavor(int id)
+    {
+      Treat selectedTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(selectedTreat);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      FlavorTreat joinTableEntry = null;
+      try
+      {
+        joinTableEntry = _db.FlavorTreat.Where(entry => entry.TreatId == treat.TreatId && entry.FlavorId == FlavorId).Single();
+      }
+      catch
+      {
+      }
+      if (FlavorId != 0 && joinTableEntry == null)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = treat.TreatId });
+    }
+
+    [Authorize]
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      FlavorTreat joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.TreatId });
     }
   }
 }
