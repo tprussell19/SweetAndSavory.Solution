307080dae986f201bdfe866f3a185cd6ed33c565
Thomas Russell <t.p.russell19@gmail.com>
2021-06-04 10:16:32 -0700

    :hammer: update homecontroller to include lists of treats and flavors

--- SweetAndSavory/Controllers/HomeController.cs
+++ SweetAndSavory/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SweetAndSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SweetAndSavory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly SweetAndSavoryContext _db;
+
+    public HomeController(SweetAndSavoryContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Treats = _db.Treats.ToList();
+      ViewBag.Flavors = _db.Flavors.ToList();
       return View();
     }
-
   }
 }
