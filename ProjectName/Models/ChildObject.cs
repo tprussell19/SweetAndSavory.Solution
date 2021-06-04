d5511e98f12076d7c34d7f0679a7a5b9b7f20ec3
Thomas Russell <80493022+tprussell19@users.noreply.github.com>
2021-06-04 08:44:02 -0700

    Initial commit

---
+++ ProjectName/Models/ChildObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectName.Models
+{
+  public class ChildObject
+  {
+    public int ChildObjectId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Location { get; set; }
+    public int ParentObjectId { get; set; }
+    public virtual ParentObject ParentObject { get; set; }
+  }
+}
