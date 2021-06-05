9de261a6a18ab7726d8ae1344f40d53bc3925f73
Thomas Russell <t.p.russell19@gmail.com>
2021-06-04 17:41:15 -0700

    :bug::hammer: add dbset flavortreat to fix build errors

--- SweetAndSavory/Models/SweetAndSavoryContext.cs
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -5,8 +5,8 @@ namespace SweetAndSavory.Models
   public class SweetAndSavoryContext : DbContext
   {
     public virtual DbSet<Flavor> Flavors { get; set; }
-    public DbSet<Treat> Treats { get; set; }
-
+    public virtual DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
     public SweetAndSavoryContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
